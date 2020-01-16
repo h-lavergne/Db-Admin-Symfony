62e39055167aad37b9a160163a8b6fc6f04d115b
Lavergne <hugo.lavergne@devinci.fr>
2020-01-16 21:46:43 +0100

    add validation as annotations in model

--- src/Entity/Food.php
+++ src/Entity/Food.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\FoodRepository")
@@ -18,11 +19,13 @@ class Food
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=3, max=15)
      */
     private $name;
 
     /**
      * @ORM\Column(type="float")
+     * @Assert\Range(min=0.1, max=100)
      */
     private $price;
 
